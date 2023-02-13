def224e5417d64d46089da7aa81c7a5b586647b5
fabriciomarote <fabrii.cai93@gmail.com>
2023-02-13 19:18:53 -0300

    Se modifica Pokemon por responsive

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -31,7 +31,7 @@ const Pokemon = ( props ) => {
     const blackHeart =  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                         </svg>;
-    const heart = pokemon != undefined ? (favoritesPokemons.includes(pokemon.name) ? redHeart : blackHeart) : '';
+    const heart = pokemon !== undefined ? (favoritesPokemons.includes(pokemon.name) ? redHeart : blackHeart) : '';
 
     const clickHeart = (event) => {
         event.preventDefault();
@@ -73,35 +73,41 @@ const Pokemon = ( props ) => {
                                             })}
                                         </div>
                                         <div className="box-bottom">     
-                                            <div className="poke-type pokemon-type">
+                                            <div className="poke-types pokemon-type">
                                                 <p className='name-date'>Tipo:</p>
-                                                { pokemon.types.map((type, idx) => {
-                                                    return (
-                                                        <div key={idx} className="poke-type-text">
-                                                            {type.type.name}
-                                                        </div>
-                                                    );
-                                                })}
+                                                <div className='dates'>
+                                                    { pokemon.types.map((type, idx) => {
+                                                        return (
+                                                            <div key={idx} className="poke-type-text">
+                                                                {type.type.name}
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
                                             </div>
-                                            <div className="poke-type pokemon-ability">
+                                            <div className="poke-types pokemon-ability">
                                                 <p className='name-date'>Habilidades:</p>
-                                                { pokemon.abilities.map((ability, idx) => {
-                                                    return (
-                                                        <div key={idx} className="poke-ability-text">
-                                                            {ability.ability.name}
-                                                        </div>
-                                                    );
-                                                })}
+                                                <div className='dates'>
+                                                    { pokemon.abilities.map((ability, idx) => {
+                                                        return (
+                                                            <div key={idx} className="poke-ability-text">
+                                                                {ability.ability.name}
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
                                             </div>
-                                            <div className="poke-type pokemon-power">
+                                            <div className="poke-types pokemon-power">
                                                 <p className='name-date'>Poderes:</p>
-                                                { pokemon.held_items.map((item, idx) => {
-                                                    return (
-                                                        <div key={idx} className="poke-power-text">
-                                                            {item.item.name}
-                                                        </div>
-                                                    );
-                                                })}
+                                                <div className='dates'>
+                                                    { pokemon.held_items.map((item, idx) => {
+                                                        return (
+                                                            <div key={idx} className="poke-power-text">
+                                                                {item.item.name}
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
                                             </div>
                                             <div className="poke-info-like"> 
                                                 <div className="poke-info-date">
@@ -131,45 +137,11 @@ const Pokemon = ( props ) => {
     }
 
     return (
-        <>
-            {render()}
-        </>
+        <div className='not-poke-favorite'>
+            { render() }
+        </div>
               
     )
 };
 
 export default Pokemon;
-
-/*
-
-<div className="pokemon-container" data-theme={theme}>
-                <div className="pokemon-image-container">
-                    <img src={pokemon.sprites.front_default} alt={pokemon.name} className="pokemon-img"/>               
-                </div>
-                <div className="pokemon-body">
-                    <div className="card-top">
-                        <p className='pokemon-name'>{pokemon.name}</p>
-                        <div className="pokemon-id">
-                            <p>N°{pokemon.id}</p>
-                        </div>
-                    </div>
-                    <div className="card-bottom">
-                        <div className="pokemon-type">
-                            {pokemon.types.map((type, idx) => {
-                                return (
-                                    <div key={idx} className="pokemon-type-text">
-                                        {type.type.name}
-                                    </div>
-                                );
-                            })}
-                        </div>
-                        <button onClick={clickHeart} className="pokemon-heart-btn">
-                            <div className="pokemon-favorite">
-                                {heart}
-                            </div>
-                        </button>    
-                    </div>
-                </div>
-            </div>
-
-*/
